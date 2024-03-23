b87135bc15c8f2a6ed6c5d4975cb93900462dd02
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-03-23 21:21:05 +0900

    Update translations-en.js

--- js/translations-en.js
+++ js/translations-en.js
@@ -7,8 +7,8 @@ const translationsEn = {
     "programming_is_fun": "Programming is fun",
     "web_developer_portfolio_site": "Web developer portfolio site",
     "about_me": "About me",
-    "junior_frontend_developer": "Junior frontend developer",
-    "intro_text": "Hi, I'm Denis, a junior frontend developer with a burning desire to create amazing user interfaces. My passion for web development inspires me to constantly learn new technologies and techniques. Ready to take on any challenge and eager to grow professionally every day.",
+    "junior_frontend_developer": "frontend developer",
+    "intro_text": "Hi, I'm Denis, a frontend developer with a burning desire to create amazing user interfaces. My passion for web development inspires me to constantly learn new technologies and techniques. Ready to take on any challenge and eager to grow professionally every day.",
     "project_opportunity": "Every project for me is an opportunity not only to apply my knowledge, but also to constantly grow in my skills. I am convinced that the most important aspect of development is the ability to solve problems and find creative approaches to the realization of tasks.",
     // Добавьте другие переводы по мере необходимости
 };
